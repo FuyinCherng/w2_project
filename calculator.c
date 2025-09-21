683bee6b8f5f84a4d70072aeed9057b28edaa397
FuyinCherng <fuyincherng@gmail.com>
2025-09-21 12:35:06 +0800

    Change calculator

--- calculator.c
+++ calculator.c
@@ -4,8 +4,13 @@ int add(int a, int b) {
     return a + b;
 }
 
+int subtract(int a, int b) {
+    return a - b;
+}
+
 int main() {
     int x = 5, y = 10;
     printf("Sum: %d\n", add(x, y));
+    printf("Difference: %d\n", subtract(x, y));
     return 0;
 }
